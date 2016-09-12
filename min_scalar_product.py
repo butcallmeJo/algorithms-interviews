93580b38457c3496c9124826276ab0218b90845d
butcallmejo <josquin.gaillard@gmail.com>
2016-09-12 15:00:08 -0700

    getting somewhere with scalar product

--- min_scalar_product.py
+++ min_scalar_product.py
@@ -19,22 +19,65 @@ for _ in range(T):
     N = int(sys.stdin.readline())
     v1 = [int(i) for i in sys.stdin.readline().split()]
     v2 = [int(i) for i in sys.stdin.readline().split()]
+    # Can refactor and make code better:
     if len(v1) > 1:
-        biggest_num = v1[0]
-        biggest_num_2 = v1[1]
-    if v1[1] > biggest_num:
-        biggest_num = v1[1]
-        biggest_num_2 = v1[0]
+        v1_biggest_num = v1[0]
+        v1_biggest_num_2 = v1[1]
+        v1_smallest_num = v1[0]
+        v1_smallest_num_2 = v1[0]
+        if v1[1] > v1_biggest_num:
+            v1_biggest_num = v1[1]
+            v1_biggest_num_2 = v1[0]
+        if v1[1] < v1_smallest_num:
+            v1_smallest_num = v1[1]
+            v1_smallest_num_2 = v1[0]
     if len(v2) > 1:
-        biggest_num = v2[0]
-        biggest_num_2 = v2[1]
-    if v2[1] > biggest_num:
-        biggest_num = v2[1]
-        biggest_num_2 = v2[0]
+        v2_biggest_num = v2[0]
+        v2_biggest_num_2 = v2[1]
+        v2_smallest_num = v2[0]
+        v2_smallest_num_2 = v2[0]
+        if v2[1] > v2_biggest_num:
+            v2_biggest_num = v2[1]
+            v2_biggest_num_2 = v2[0]
+        if v2[1] < v2_smallest_num:
+            v2_smallest_num = v2[1]
+            v2_smallest_num_2 = v2[0]
 
     for i in v1:
-        if i > biggest_num:
-            biggest_num = i
-        elif i > biggest_num_2:
-            biggest_num_2 = i
-        
+        if i > v1_biggest_num:
+            v1_biggest_num = i
+        elif i > v1_biggest_num_2:
+            v1_biggest_num_2 = i
+        elif i < v1_smallest_num:
+            v1_smallest_num = i
+        elif i < v1_smallest_num_2:
+            v1_smallest_num_2 = i
+
+    for i in v2:
+        if i > v2_biggest_num:
+            v2_biggest_num = i
+        elif i > v2_biggest_num_2:
+            v2_biggest_num_2 = i
+        elif i < v2_smallest_num:
+            v2_smallest_num = i
+        elif i < v2_smallest_num_2:
+            v2_smallest_num_2 = i
+
+    print v1_biggest_num, v1_biggest_num_2, v1_smallest_num, v1_smallest_num_2
+    print v2_biggest_num, v2_biggest_num_2, v2_smallest_num, v2_smallest_num_2
+
+'''
+test inputs:
+
+1:
+2\n3\n1 3 -5\n-2 4 1\n5\n1 2 3 4 5\n1 0 1 0 1
+
+2
+3
+1 3 -5
+-2 4 1
+5
+1 2 3 4 5
+1 0 1 0 1
+
+'''
