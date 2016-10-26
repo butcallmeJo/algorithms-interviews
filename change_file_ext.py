b82b671ccf5dfc67754462d946da393dbceda743
butcallmejo <josquin.gaillard@gmail.com>
2016-10-26 10:26:57 -0700

    redid change file ext for practice

--- change_file_ext.py
+++ change_file_ext.py
@@ -1,14 +1,19 @@
 import os
 
 '''
-This program is dangerous and could change some files drastically. DO NOT RUN unless you know what you are doing.
+This program is dangerous and could change some files drastically. DO NOT RUN
+unless you know what you are doing.
+
+Change the file extension depending on DIRECTORY and FILE_EXT
 '''
 
-directory = os.listdir('.') # looks inside current directory
-ext_to_replace = 'doc' # replace this with whatever extension you want
-new_ext = 'txt' # replace this with whatever extension you want
+PATH = "/tmp/test/" # Specify path here
+DIRECTORY = os.listdir(PATH) # List of files in dir
+FILE_EXT = ".doc" # Specify which extension to replace by NEW_EXT
+NEW_EXT = ".pdf" # Specify which extension to replace to
 
-for file in directory:
-    if file.endswith(ext_to_replace):
-        basename = file.split('.')[:-1:] # splitting the full name at each '.' and not keeping track of the last 'extension' 
-        os.rename(file, '.'.join(basename) + '.' + new_ext) # renames files from original to new concatenated version
+for filename in DIRECTORY: # Iterate over the list of filenames
+    if filename.endswith(FILE_EXT): # If filename has the extension needed..
+        no_ext = filename[:-(len(FILE_EXT))] # Gets the basename
+        # print no_ext + NEW_EXT # To test what has been changed
+        os.rename((PATH + filename), (PATH + no_ext + NEW_EXT)) # Renames file
